3b52ed02640ddb22d623abca90f5f160d084097a
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-06-11 17:51:19 +0530

    url fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://vishnusanal.github.io/',
+  url: 'https://openmobilehub.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/developer-multipaz-website/',
