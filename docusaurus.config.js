94d2a445b55fc947a3d1ce3180727a3029dd8b65
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-06-12 12:39:50 +0530

    url & baseurl from env

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,8 @@
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
+require('dotenv').config();
+
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
@@ -19,11 +21,8 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://openmobilehub.github.io/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/developer-multipaz-website/',
+  url: process.env.DOCUSAURUS_URL || 'http://localhost',
+  baseUrl: process.env.DOCUSAURUS_BASEURL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
