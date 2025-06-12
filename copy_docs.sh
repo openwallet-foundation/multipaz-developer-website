19b02e05d7c41c2ff62a5f49be0d6314a61f0277
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-06-12 22:20:10 +0530

    fix file paths

--- copy_docs.sh
+++ copy_docs.sh
@@ -4,15 +4,15 @@ DOCUSAURUS_REPO="docusaurus-repo"
 cp -R $KOTLIN_REPO/build/dokka/htmlMultiModule/* $DOCUSAURUS_REPO/static/kdocs/
 echo "KDocs copied to Docusaurus static/api directory"
 
-cp $KOTLIN_REPO/README.md                        $DOCUSAURUS_REPO/overview/0-overview.md
-cp $KOTLIN_REPO/CHANGELOG.md                     $DOCUSAURUS_REPO/changelog/0-changelog.md
+cp $KOTLIN_REPO/README.md                   $DOCUSAURUS_REPO/overview/0-overview.md
+cp $KOTLIN_REPO/CHANGELOG.md                $DOCUSAURUS_REPO/changelog/0-changelog.md
 
-cp $KOTLIN_REPO/docs/contributing.md             $DOCUSAURUS_REPO/contributing/0-contributing.md
-cp $KOTLIN_REPO/docs/code-of-conduct.md          $DOCUSAURUS_REPO/contributing/1-code-of-conduct.md
-cp $KOTLIN_REPO/docs/CODING-STYLE.md             $DOCUSAURUS_REPO/contributing/2-coding-style.md
-cp $KOTLIN_REPO/docs/DEVELOPER-ENVIRONMENT.md    $DOCUSAURUS_REPO/contributing/3-developer-environment.md
-cp $KOTLIN_REPO/docs/TESTING.md                  $DOCUSAURUS_REPO/contributing/4-testing.md
-cp $KOTLIN_REPO/docs/MAINTAINERS.md              $DOCUSAURUS_REPO/contributing/5-maintainers.md
+cp $KOTLIN_REPO/docs/contributing.md        $DOCUSAURUS_REPO/contributing/0-contributing.md
+cp $KOTLIN_REPO/docs/code-of-conduct.md     $DOCUSAURUS_REPO/contributing/1-code-of-conduct.md
+cp $KOTLIN_REPO/CODING-STYLE.md             $DOCUSAURUS_REPO/contributing/2-coding-style.md
+cp $KOTLIN_REPO/DEVELOPER-ENVIRONMENT.md    $DOCUSAURUS_REPO/contributing/3-developer-environment.md
+cp $KOTLIN_REPO/TESTING.md                  $DOCUSAURUS_REPO/contributing/4-testing.md
+cp $KOTLIN_REPO/MAINTAINERS.md              $DOCUSAURUS_REPO/contributing/5-maintainers.md
 echo "Markdown files copied successfully"
 
 # Replace auto links with []() links in markdown files
